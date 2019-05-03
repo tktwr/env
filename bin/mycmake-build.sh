b3ee6b06c3d9910e7cdbd105aa4585c839cc39bf
tktwr <takehiro.tawara@gmail.com>
2019-05-03 15:33:01 +0900

    support ninja build

---
+++ bin/mycmake-build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+f_mycmake_build() {
+  local generator=$1
+  local config=$2
+
+  local build_dir
+  local opt
+
+  case $generator in
+    ninja)
+      build_dir=build.$generator/$config
+      ;;
+    unix)
+      build_dir=build.$generator/$config
+      opt="$opt -j10"
+      ;;
+    vs2017)
+      build_dir=build.$generator
+      ;;
+    *)
+      echo "no such generator: $generator"
+      return
+      ;;
+  esac
+
+  echo "cmake --build $build_dir --config $config $opt"
+  cmake --build $build_dir --config $config $opt
+}
+
+f_mycmake_build $@
+
---
+++ bin/mycmake.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+f_mycmake() {
+  local generator=$1
+  local config=$2
+
+  local generator_name
+  local build_dir
+  local opt
+
+  case $generator in
+    ninja)
+      generator_name="Ninja"
+      build_dir=build.$generator/$config
+      opt="$opt -DCMAKE_BUILD_TYPE=$config"
+      ;;
+    unix)
+      generator_name="Unix Makefiles"
+      build_dir=build.$generator/$config
+      opt="$opt -DCMAKE_BUILD_TYPE=$config"
+      ;;
+    vs2017)
+      generator_name="Visual Studio 15 2017 Win64"
+      build_dir=build.$generator
+      ;;
+    *)
+      echo "no such generator: $generator"
+      return
+      ;;
+  esac
+
+  opt="$opt -DCMAKE_INSTALL_PREFIX=$MY_OPT/usr/local"
+  opt="$opt -DCMAKE_EXPORT_COMPILE_COMMANDS=YES"
+
+  echo "cmake -S. -B$build_dir -G\"$generator_name\" $opt"
+  cmake -S. -B$build_dir -G"$generator_name" $opt
+}
+
+f_mycmake $@
+
