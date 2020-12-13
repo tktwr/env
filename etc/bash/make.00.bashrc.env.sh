7ee5330f3efccae75d302cd8d80bd7cad031e378
tktwr <takehiro.tawara@gmail.com>
2020-12-13 14:19:34 +0900

    rearrange make-bashrc files

--- etc/bash/make.00.bashrc.env.sh
+++ etc/bash/make.00.bashrc.env.sh
@@ -6,9 +6,9 @@ _MY_LOCAL_FZY=$_MY_CONFIG/tktwr.local/env/etc/fzy
 _MY_PRIVATE_FZY=$_MY_CONFIG/tktwr.private/env/etc/fzy
 
 if [ ! -z "$USERPROFILE" ]; then
-  ./make-dir.py $_MY_REMOTE_FZY/dir.00.msys.txt
+  ./make.dir.py $_MY_REMOTE_FZY/dir.00.msys.txt
 else
-  ./make-dir.py $_MY_REMOTE_FZY/dir.00.unix.txt
+  ./make.dir.py $_MY_REMOTE_FZY/dir.00.unix.txt
 fi
 
 cat bashrc.env
@@ -21,10 +21,10 @@ $_MY_PRIVATE_FZY \
 "
 
 for i in $DIRS; do
-  ./make-dir.py $i/dir.01.usr.txt
+  ./make.dir.py $i/dir.01.usr.txt
 done
 
 for i in $DIRS; do
-  ./make-dir.py $i/dir.txt
+  ./make.dir.py $i/dir.txt
 done
 
