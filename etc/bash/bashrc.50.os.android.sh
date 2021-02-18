3757b767172e44ac9083d80aec37da397dd0174c
tktwr <takehiro.tawara@gmail.com>
2021-02-18 13:31:14 +0900

    update

--- etc/bash/bashrc.50.os.android.sh
+++ etc/bash/bashrc.50.os.android.sh
@@ -4,5 +4,23 @@
 # android
 #======================================================
 
+#------------------------------------------------------
+# python venv
+#------------------------------------------------------
+
+export MY_PYTHON_EXE=python3
+
+mypython-venv-activate() {
+  source $MY_PYTHON_VENV_DIR/$1/bin/activate
+  MY_PYTHON_VENV="$1"
+}
+
+mypython-venv-deactivate() {
+  deactivate
+  MY_PYTHON_VENV=""
+}
+
+#------------------------------------------------------
+
 alias ls='ls -F'
 
