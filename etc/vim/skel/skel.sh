dedbc650e27f18b46cfbaff32f25b54fba1b5847
tktwr <takehiro.tawara@gmail.com>
2021-04-01 09:43:20 +0900

    update

--- etc/vim/skel/skel.sh
+++ etc/vim/skel/skel.sh
@@ -2,6 +2,8 @@
 
 bin_name=`basename $0`
 
+g_args=""
+
 f_help() {
   echo "NAME"
   echo "  $bin_name"
@@ -10,18 +12,30 @@ f_help() {
   echo "  $bin_name [options]"
   echo
   echo "OPTIONS"
-  echo "  -h, --help ... print help"
+  echo "  -h, --help            ... print help"
+  echo "  --filepath <filepath> ... filepath"
 }
 
 f_args() {
-  for i in "$@"; do
-    case "$i" in
+  while [ $# -gt 0 ]; do
+    case "$1" in
       -h|--help)
         f_help
         exit
         ;;
+      --filepath)
+        shift
+        p=$(cygpath -au $1)
+        g_args="$g_args $p"
+        ;;
+      *)
+        g_args="$g_args $1"
+        ;;
     esac
+    shift
   done
 }
 
 f_args "$@"
+echo "g_args: $g_args"
+
