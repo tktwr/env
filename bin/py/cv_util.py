7d5fd9c1bc5f694823f696c3a4b3415ac9a114a8
tktwr <takehiro.tawara@gmail.com>
2021-09-28 13:40:47 +0900

    update

--- bin/py/cv_util.py
+++ bin/py/cv_util.py
@@ -19,3 +19,28 @@ def cv_resize(ifname, ofname, dst_size):
     print(f"cv_resize {ifname} {ofname} {new_size}")
 
 
+class ImageTile():
+    def __init__(self):
+        pass
+
+    def hconcat(self, img_list, interpolation=cv2.INTER_AREA):
+        l = []
+        for i in img_list:
+            if len(i.shape) == 3:
+                l.append(i)
+            else:
+                bgr = cv2.cvtColor(i, cv2.COLOR_GRAY2BGR)
+                l.append(bgr)
+        h_min = min(i.shape[0] for i in l)
+        img_list_resize = [cv2.resize(i, (int(i.shape[1] * h_min / i.shape[0]), h_min), interpolation=interpolation) for i in l]
+        return cv2.hconcat(img_list_resize)
+
+    def vconcat(self, img_list, interpolation=cv2.INTER_AREA):
+        w_min = min(i.shape[1] for i in img_list)
+        img_list_resize = [cv2.resize(i, (w_min, int(i.shape[0] * w_min / i.shape[1])), interpolation=interpolation) for i in img_list]
+        return cv2.vconcat(img_list_resize)
+
+    def tile(self, img_list_2d, interpolation=cv2.INTER_AREA):
+        img_list_v = [self.hconcat(img_list_h, interpolation) for img_list_h in img_list_2d]
+        return self.vconcat(img_list_v, interpolation)
+
