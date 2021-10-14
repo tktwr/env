3de1038b7304631d36e5acdebc04c603c84119cc
tktwr <takehiro.tawara@gmail.com>
2021-10-14 15:55:09 +0900

    update

--- bin/gfx/image-show.py
+++ bin/gfx/image-show.py
@@ -3,11 +3,26 @@
 
 import sys
 import cv2
+import cv_util as cu
 
 
 def img_show(fname):
     img = cv2.imread(fname, cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
-    cv2.imshow(f"{fname}", img)
+    h, w = img.shape[:2]
+    pos = (w//2, h//2)
+    val = img[pos[1], pos[0]]
+    h_max = 500
+    w_max = 500
+    if h >= w and h > h_max:
+        dst_size = (0, h_max)
+        oimg = cu.cv_resize_img(img, dst_size)
+    elif w >= h and w > w_max:
+        dst_size = (w_max, 0)
+        oimg = cu.cv_resize_img(img, dst_size)
+    else:
+        dst_size = (0, 0)
+        oimg = img
+    cv2.imshow(f"{fname}, orig={(w, h)}, dst={dst_size}, pos={pos}, val={val}", oimg)
 
 
 if __name__ == "__main__":
--- bin/py/cv_util.py
+++ bin/py/cv_util.py
@@ -34,14 +34,18 @@ def cv_pick(fname, x, y):
     return img[y, x]
 
 
-def cv_resize(ifname, ofname, dst_size):
-    img = cv2.imread(ifname, cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
+def cv_resize_img(img, dst_size):
     h, w = img.shape[:2]
 
     img_size = ImageSize((w, h))
     new_size = img_size.getSize(dst_size)
 
-    oimg = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+    return cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+
+
+def cv_resize(ifname, ofname, dst_size):
+    img = cv2.imread(ifname, cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
+    oimg = cv_resize_img(img, dst_size)
     cv2.imwrite(ofname, oimg)
 
     print(f"cv_resize {ifname} {ofname} {new_size}")
