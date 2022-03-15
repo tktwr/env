4b67b3366448669c93650d90aa65387fa49d2ce0
tktwr <takehiro.tawara@gmail.com>
2022-03-15 23:48:12 +0900

    set XDG env only on linux

--- etc/bash/bashrc.50.os.linux.sh
+++ etc/bash/bashrc.50.os.linux.sh
@@ -4,6 +4,14 @@
 # linux
 #======================================================
 
+export XDG_CONFIG_HOME="~/.config"
+export XDG_DATA_HOME="~/.local/share"
+export XDG_CACHE_HOME="~/.cache"
+
+#------------------------------------------------------
+# alias
+#------------------------------------------------------
+
 #alias x='nautilus'
 #alias p='evince'
 alias x='exo-open --launch FileManager'
