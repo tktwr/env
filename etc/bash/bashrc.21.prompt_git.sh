9550ffcf6ed083a648f2af6cc583a630d75fc8a2
tktwr <takehiro.tawara@gmail.com>
2022-08-22 22:45:45 +0900

    update

--- etc/bash/bashrc.21.prompt_git.sh
+++ etc/bash/bashrc.21.prompt_git.sh
@@ -2,46 +2,34 @@
 
 f_get_prompt_color_host() {
   local ECO
-  case $HOSTNAME in
-    *)
-      ECO=$ECO6
-      ;;
+  case $MY_HOST_NAME in
+    *) ECO=$ECO2 ;;
   esac
-  echo $ECO$HOSTNAME
+  echo $ECO$MY_HOST_NAME
 }
 
 f_get_prompt_color_os() {
   local ECO
   case $MY_OS_NAME in
-    gitbash)
-      ECO=$ECO1
-      ;;
-    msys)
-      ECO=$ECO2
-      ;;
-    wsl)
-      ECO=$ECO3
-      ;;
-    termux)
-      ECO=$ECO4
-      ;;
-    *)
-      ECO=$ECO6
-      ;;
+    gitbash) ECO=$ECO1 ;;
+    msys)    ECO=$ECO2 ;;
+    wsl)     ECO=$ECO7 ;;
+    termux)  ECO=$ECO4 ;;
+    *)       ECO=$ECO6 ;;
   esac
-  echo $ECOR$ECO$MY_OS_NAME$ECOD
+  echo $ECO$MY_OS_NAME
 }
 
 f_set_prompt_git() {
   source "$MY_ENV/etc/git/git-prompt.sh"
   source "$MY_ENV/etc/git/git-completion.bash"
 
-  local PS1_HOST=$(f_get_prompt_color_host)
-  local PS1_OS=$(f_get_prompt_color_os)
+  #local PS1_HOST=$(f_get_prompt_color_host)
+  #local PS1_OS=$(f_get_prompt_color_os)
   local PS1_DIR="$ECO3[\w]"
-  local PS1_GIT=$ECO6'`__git_ps1`'
+  local PS1_GIT=$ECO6'$(__git_ps1 "(%s)")'
 
   export PROMPT_DIRTRIM=3
-  export PS1="$PS1_HOST:$PS1_OS$PS1_DIR$PS1_GIT$ECOD\$ "
+  export PS1="$PS1_DIR$PS1_GIT$ECOD\$ "
 }
 
