eb96adc99a8fd37ad74f959a03c5c9fa5d1a7959
tktwr <takehiro.tawara@gmail.com>
2022-10-23 13:12:14 +0900

    update

--- bin/test/test-pathconv.sh
+++ bin/test/test-pathconv.sh
@@ -5,16 +5,21 @@ $HOME/.bashrc \
 $HOME/WinHome/.bashrc \
 $HOME/.not_existing_file \
 $HOME/WinHome/.not_existing_file \
+C:/Users/Takeh/.bashrc \
+C:/Users/Takeh2/.bashrc \
 "
 
-echo "--- pathconv.sh ---"
+echo "=== pathconv.sh ===================="
 for i in $files; do
-  pathconv.sh unix $i
-  pathconv.sh win $i
+  echo "orig: $i --------------------"
+  echo "unix: $(pathconv.sh unix $i)"
+  echo "win : $(pathconv.sh win $i)"
 done
 
-echo "--- pathconv.py ---"
+echo "=== pathconv.py ===================="
 for i in $files; do
-  pathconv.py -t unix $i
-  pathconv.py -t windows $i
+  echo "orig: $i --------------------"
+  echo "unix: $(pathconv.py -t unix $i)"
+  echo "win : $(pathconv.py -t windows $i)"
+  echo "mix : $(pathconv.py -t mixed $i)"
 done
