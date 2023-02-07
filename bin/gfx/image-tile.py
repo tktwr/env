5d81393faa6113fcb91b184815e518e210abb61d
tktwr <takehiro.tawara@gmail.com>
2023-02-08 00:31:01 +0900

    update

--- bin/gfx/image-tile.py
+++ bin/gfx/image-tile.py
@@ -7,8 +7,8 @@ import tt_util as tu
 import cv_util as cu
 
 
-def make_img_list(ifnames):
-    blank_img = np.zeros((500, 500, 3), 'uint8')
+def make_img_list(wh, ifnames):
+    blank_img = np.zeros((wh[1], wh[0], 3), 'uint8')
 
     l1 = []
     for i in ifnames:
@@ -16,20 +16,22 @@ def make_img_list(ifnames):
             img = blank_img
         else:
             img = cu.cv_load(i)
+            img = cu.cv_fit_img(img, wh)
+            img = cu.cv_crop_img(img, (0, 0), wh, True)
         l1.append(img)
 
     return l1
 
 
-def make_vtile(cols, ifnames):
-    l1 = make_img_list(ifnames)
+def make_vtile(tile_wh, cols, ifnames):
+    l1 = make_img_list(tile_wh, ifnames)
     rows = len(l1) // cols
     l2 = cu.cv_convert_1d_to_2d(l1, rows)
     return cu.cv_vtile(l2)
 
 
-def make_htile(cols, ifnames):
-    l1 = make_img_list(ifnames)
+def make_htile(tile_wh, cols, ifnames):
+    l1 = make_img_list(tile_wh, ifnames)
     l2 = cu.cv_convert_1d_to_2d(l1, cols)
     return cu.cv_htile(l2)
 
@@ -47,6 +49,11 @@ def parse_args():
                         type=str,
                         default='tile.png',
                         help="set output file name")
+    parser.add_argument('--tile_wh',
+                        type=int,
+                        nargs=2,
+                        default=[500, 500],
+                        help='set each tile size')
     parser.add_argument('-c', '--cols',
                         type=int,
                         default=2,
@@ -66,9 +73,8 @@ if __name__ == "__main__":
     args = parse_args()
 
     if args.htile:
-        img = make_htile(args.cols, args.files)
+        img = make_htile(args.tile_wh, args.cols, args.files)
     else:
-        img = make_vtile(args.cols, args.files)
+        img = make_vtile(args.tile_wh, args.cols, args.files)
 
     cu.cv_save(args.output, img)
-
