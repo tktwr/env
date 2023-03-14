714f5111b399c79a8ba636c22e5c01b83e499d96
tktwr <takehiro.tawara@gmail.com>
2023-03-15 07:21:32 +0900

    update

--- bin/ls.sh
+++ bin/ls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /usr/bin/ls -v -F --color=auto -I '{*}' -I 'NTUSER.*' -I 'ntuser.*' -I '*.lnk' -I '*.url' -I 'desktop.ini' "$@"
+exec /bin/ls -v -F --color=auto -I '{*}' -I 'NTUSER.*' -I 'ntuser.*' -I '*.lnk' -I '*.url' -I 'desktop.ini' "$@"
