6a45ad96d3d2d12cd08e5742d5c985df56af0965
tktwr <takehiro.tawara@gmail.com>
2023-06-19 22:04:11 +0900

    update clip and paste

--- bin/os.wsl/clip.sh
+++ bin/os.wsl/clip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps_file="$MY_LOCAL_CONFIG_WIN/bin/paste.ps1"
+ps_file="$MY_LOCAL_CONFIG/bin/paste.ps1"
 
 if [ -f $ps_file ]; then
   iconv -f utf8 -t utf16 | clip.exe
--- bin/os.wsl/paste.sh
+++ bin/os.wsl/paste.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ps_file="$MY_LOCAL_CONFIG_WIN/bin/paste.ps1"
+ps_file="$MY_LOCAL_CONFIG/bin/paste.ps1"
+ps_file_win="$MY_LOCAL_CONFIG_WIN/bin/paste.ps1"
 
 if [ -f $ps_file ]; then
-  powershell.exe $ps_file
+  powershell.exe $ps_file_win
 else
   powershell.exe -OutputFormat Text -Command Get-Clipboard
 fi
