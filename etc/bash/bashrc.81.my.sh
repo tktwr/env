05830dbbd739f8f9d5a78f3f7682e3f7714e54c1
tktwr <takehiro.tawara@gmail.com>
2023-09-18 20:13:36 +0900

    update alias

--- etc/bash/bashrc.81.my.sh
+++ etc/bash/bashrc.81.my.sh
@@ -26,8 +26,37 @@ mycmake-set() {
 #------------------------------------------------------
 # vim terminal
 #------------------------------------------------------
-if [ "$VIM_TERMINAL" ]; then
+f_alias_vim_term() {
+  alias ,F='vimapi-edit-dir'
+  alias ,F2='vimapi-edit-dir-2'
+
+  alias ,I='vimapi_exec VisIDE'
+  alias ,I2='vimapi_exec VisIDE 2'
+  alias ,It='vimapi_exec_tab VisIDE'
+
+  alias ,N='vimapi_exec new'
+  alias ,Nv='vimapi_exec vnew'
+  alias ,Nt='vimapi_exec tabedit'
+
+  alias ,T='vimapi_exec VisTerm'
+  alias ,Tv='vimapi_exec VisTermV'
+  alias ,Tt='vimapi_exec tab VisTerm'
+
+  alias ,resize='vimapi_exec VisWinResize'
+  alias ,resize_x='vimapi_exec VisWinVResize'
+  alias ,toggle='vimapi_exec VisWinMaximizeYToggleTerm'
+  alias ,toggle_x='vimapi_exec VisWinMaximizeXToggleTerm'
+
+  alias ,lg='vimapi_exec VisLazygit tab'
+  alias ,man='vimapi_exec VisFzfMan tab'
+  alias ,apt='vimapi_exec VisFzfApt tab'
+  alias ,pip='vimapi_exec VisFzfPip tab'
+  alias ,dpkg='vimapi_exec VisFzfDpkg tab'
+
+  alias ,GS='vimapi_exec VisGS'
+  alias ,GV='vimapi_exec VisGV'
   alias ,GL='vimapi_exec_above GitLog $PWD'
+
   alias ,t=',toggle'
   alias ,tx=',toggle_x'
   alias ,t0=',resize 5'
@@ -35,7 +64,28 @@ if [ "$VIM_TERMINAL" ]; then
   alias ,t2=',resize 20'
   alias ,t3=',resize 30'
   alias ,t4=',resize 40'
-fi
+}
+
+f_alias_term() {
+  if [ $MY_OS_NAME = 'gitbash' ]; then
+    export TERM_cygwin='TERM=cygwin'
+  else
+    export TERM_cygwin=''
+  fi
+  alias ,I='$TERM_cygwin vim -c "VisIDE"'
+  alias ,I2='$TERM_cygwin vim -c "VisIDE 2"'
+  alias ,T='$TERM_cygwin vim -c "VisTerm" -c "only"'
+  alias ,T2='$TERM_cygwin vim -c "VisTerm" -c "only" -c "VisTerm"'
+  alias ,T2v='$TERM_cygwin vim -c "VisTerm" -c "only" -c "vert VisTerm"'
+
+  alias ,nI='nvim -c "VisIDE"'
+  alias ,nT='nvim -c "VisTerm" -c "only"'
+  alias ,nT2='nvim -c "VisTerm" -c "only" -c "VisTerm"'
+  alias ,nT2v='nvim -c "VisTerm" -c "only" -c "vert VisTerm"'
+
+  alias ,GS='vim -c "VisGS"'
+  alias ,GV='vim -c "VisGV"'
+}
 
 #======================================================
 # os
@@ -67,4 +117,19 @@ f_alias_termux() {
   :
 }
 
+f_alias_common() {
+  alias vim='vim.sh'
+  alias vimdiff='vimdiff.sh'
+  alias vimdirdiff='vimdirdiff.sh'
+  alias vim-which='vim-which.sh'
+  alias vim-where='vim-which.sh'
+}
+
+#------------------------------------------------------
+f_alias_common
 f_alias_$MY_OS_NAME
+if [ "$VIM_TERMINAL" ]; then
+  f_alias_vim_term
+else
+  f_alias_term
+fi
