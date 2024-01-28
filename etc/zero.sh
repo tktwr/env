5ca9a60897cb687a0a96eb7d3803001a5a3ce144
tktwr <takehiro.tawara@gmail.com>
2024-01-28 09:40:16 +0900

    add zero.sh

---
+++ etc/zero.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+f_zero_init() {
+  mkdir -p $HOME/.my
+  mkdir -p $HOME/.mycache
+}
+
+f_zero_bash() {
+  cd bash
+  ./make_hostname.sh > $HOME/.my/hostname.sh
+  cat bashrc_zero.sh > $HOME/.bashrc
+  cd ..
+}
+
+f_zero_vim() {
+  cd vim
+  cat vimrc_zero.vim > $HOME/.vimrc
+  cd ..
+}
+
+f_zero_init
+f_zero_bash
+f_zero_vim
