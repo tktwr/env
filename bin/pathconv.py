006ad6c34ba77b7093ef07416cf71447a52455cd
tktwr <takehiro.tawara@gmail.com>
2024-09-30 21:35:11 +0900

    update

--- bin/pathconv.py
+++ bin/pathconv.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
 import tt_util as tu
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='path conversion')
-    parser.add_argument('-v', '--verbose',
-                        action='store_true',
-                        help='show verbose message')
-    parser.add_argument('-t', '--type',
-                        type=str,
-                        choices=['unix', 'mixed', 'windows'],
-                        default='unix',
-                        help='set output type')
-    parser.add_argument('-p', '--prefix',
-                        type=str,
-                        default='/mnt',
-                        help='set prefix')
-    parser.add_argument('files',
-                        type=str,
-                        nargs='*',
-                        help='input files')
+    parser = tu.parser('path conversion')
+    A = parser.add_argument
+
+    A('files' , nargs='*'   , help='input files'          , type=str            , )
+    A('-v'    , '--verbose' , help='show verbose message' , action='store_true' , )
+    A('-t'    , '--type'    , help='set output type'      , type=str            , default='unix'  , choices=['unix' , 'mixed' , 'windows'])
+    A('-p'    , '--prefix'  , help='set prefix'           , type=str            , default='/mnt')
+
     return parser.parse_args()
-
-
-def print_args(args):
-    print(f"--- args ---")
-    print(f"args.type   : {args.type}")
-    print(f"args.prefix : {args.prefix}")
-    print(f"args.files  : {args.files}")
 
 
 if __name__ == "__main__":
     args = parse_args()
     if args.verbose:
-        print_args(args)
+        tu.print_args(args)
 
     otype  = args.type
     prefix = args.prefix
