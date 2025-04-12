d764ba3e583f98969cf258bf51d3ea1894c94337
tktwr <takehiro.tawara@gmail.com>
2025-04-12 11:03:56 +0900

    update

--- etc/bash/bashrc.40.python.sh
+++ etc/bash/bashrc.40.python.sh
@@ -18,7 +18,6 @@ _my_setup_python_win() {
 
 _my_setup_conda_win() {
   export PATH="$APP_L_MINIFORGE3/condabin:$PATH"
-  eval '$("$APP_L_MINIFORGE3_WIN/Scripts/conda.exe" "shell.bash" "hook")'
 }
 
 _my_setup_python_linux() {
