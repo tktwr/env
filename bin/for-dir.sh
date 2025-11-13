0d8387a05c76af2449629a04ba0082486bd2610a
tktwr <takehiro.tawara@gmail.com>
2025-11-13 16:23:17 +0900

    update

--- bin/for-dir.sh
+++ bin/for-dir.sh
@@ -2,7 +2,29 @@
 
 bin_name=`basename $0`
 top_dir=`pwd`
+g_esc=1
 
+f_print_dir() {
+  if [ $g_esc -eq 1 ]; then
+    line="\\e[33m$*\\e[0m"
+    line=$(echo "$line" | sed 's/rconfig/\\e[32m&\\e[33m/')
+    line=$(echo "$line" | sed 's/lconfig/\\e[34m&\\e[33m/')
+    line=$(echo "$line" | sed 's/pconfig/\\e[35m&\\e[33m/')
+    echo -e "$line"
+  else
+    echo "$*"
+  fi
+}
+f_print_cmd() {
+  if [ $g_esc -eq 1 ]; then
+    echo -e "\e[31m$*\e[0m"
+  else
+    echo "$*"
+  fi
+}
+f_print_entry() {
+  echo -e "=== [$(f_print_dir $1)]($(f_print_cmd $2)) ==="
+}
 f_exec_in_dirs() {
   cmd="$1"
   shift
@@ -10,7 +32,7 @@ f_exec_in_dirs() {
 
   for i in $dirs; do
     if [ -d $i ]; then
-      echo "=== [$i]($cmd) ==="
+      f_print_entry "$i" "$cmd"
       cd "$i"
       eval "$cmd"
       cd "$top_dir"
