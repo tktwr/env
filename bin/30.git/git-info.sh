c24406b6c639d1ce61736b70a41f4c7646e0662c
tktwr <takehiro.tawara@gmail.com>
2026-01-05 16:40:13 +0900

    update

--- bin/30.git/git-info.sh
+++ bin/30.git/git-info.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+source func_git.sh
+
 _f_git_graph() {
   local mark=""
-  local out=$(git.sh graph --color=never -1 "$@")
-  cmd.sh f_git_need_action > /dev/null
+  f_git_need_action > /dev/null
   local st=$?
   case $st in
     0) mark="[\\e[32m✔ \\e[0m]" ;; # clean
@@ -12,6 +13,7 @@ _f_git_graph() {
     3) mark="[\\e[35m✘ \\e[0m]" ;; # need_commit need_push
   esac
   if [ $st -gt 0 ]; then
+    local out=$(git.sh graph --color=never -1 "$@")
     echo -e "$mark $out"
     git.sh status -s
   fi
